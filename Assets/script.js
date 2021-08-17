20559703095e22ab43303cdcdce4d8fe13cbb37a
Rayna Ratcliff <raynaratcliff@hotmail.com>
2021-08-17 16:15:56 -0400

    declared variables

---
+++ Assets/script.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+// Declared variables
+var score = 0;
+var questionIndex = 0;
+
+// Start working code 
+// Declared variables
+var currentTime = document.getElementById("currentTime");
+var timer = document.getElementById("start");
+var questionsDiv = document.getElementById("quiz-section");
+var wrapper = document.getElementById("wrapper");
+
+// Seconds left is 15 seconds per question:
+var secondsLeft = 76;
+// Holds interval time
+var holdInterval = 0;
+// Holds penalty time
+var penalty = 10;
+// Creates new element
+var ulCreate = document.createElement("ul");
